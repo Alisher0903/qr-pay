e75e3c128eef7e9fd9d00c79aa3e4ba7de2f815a
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-08-21 21:45:57 +0500

    sq

--- src/views/admin/dataTables/index.jsx
+++ src/views/admin/dataTables/index.jsx
@@ -13,7 +13,6 @@ import {
 import tableDataDevelopment from "views/admin/dataTables/variables/tableDataDevelopment.json";
 import tableDataCheck from "views/admin/dataTables/variables/tableDataCheck.json";
 import tableDataColumns from "views/admin/dataTables/variables/tableDataColumns.json";
-import tableDataComplex from "views/admin/dataTables/variables/tableDataComplex.json";
 import React from "react";
 
 export default function Settings() {
