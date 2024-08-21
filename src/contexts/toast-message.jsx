2acde5d0720dd78a74b4c2fa4758e4aaa967453c
Alisher <alishersodiqov09@gmail.com>
2024-08-22 01:58:24 +0500

    chiqqan api lar

--- src/contexts/toast-message.jsx
+++ src/contexts/toast-message.jsx
@@ -2,4 +2,5 @@ import toast from "react-hot-toast";
 
 export const toastMessage = code => {
     if (+code === 3) return toast.error('')
+    else if (+code === 2) return toast.error('User not found')
 }
