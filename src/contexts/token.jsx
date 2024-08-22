422e1007fe860d932e0cc3f35a1263ab7032b5eb
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-08-22 12:02:28 +0500

    cq

--- src/contexts/token.jsx
+++ src/contexts/token.jsx
@@ -1,15 +1,19 @@
 export const config = {
     headers: {
-        Authorization: localStorage.getItem('token'),
+        // Authorization: localStorage.getItem('token'),
+        Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI5NzIyMjA3OTAiLCJpYXQiOjE3MjQzMDU1NDcsImV4cCI6MTgxMDcwNTU0N30.Hc88hX0fz37yHFXG31Etmfqp7JB6TxoQlG0j5bZkmPk-ITFI_Ncv4sgVawJ-7TF83_po6xBmpNOnwkGc-Jtiyg',
     }
 }
 
 export const imgConfig = {
     headers: {
         'multipart/type': 'multipart/form-data',
-        Authorization: localStorage.getItem('token'),
+        // Authorization: localStorage.getItem('token'),
+        Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI5NzIyMjA3OTAiLCJpYXQiOjE3MjQzMDU1NDcsImV4cCI6MTgxMDcwNTU0N30.Hc88hX0fz37yHFXG31Etmfqp7JB6TxoQlG0j5bZkmPk-ITFI_Ncv4sgVawJ-7TF83_po6xBmpNOnwkGc-Jtiyg',
     }
 }
 
-export const setConfig = () => config.headers.Authorization = localStorage.getItem('token')
-export const setImgConfig = () => imgConfig.headers.Authorization = localStorage.getItem('token')
+// export const setConfig = () => config.headers.Authorization = localStorage.getItem('token')
+// export const setImgConfig = () => imgConfig.headers.Authorization = localStorage.getItem('token')
+export const setConfig = () => config.headers.Authorization = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI5NzIyMjA3OTAiLCJpYXQiOjE3MjQzMDU1NDcsImV4cCI6MTgxMDcwNTU0N30.Hc88hX0fz37yHFXG31Etmfqp7JB6TxoQlG0j5bZkmPk-ITFI_Ncv4sgVawJ-7TF83_po6xBmpNOnwkGc-Jtiyg'
+export const setImgConfig = () => imgConfig.headers.Authorization = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI5NzIyMjA3OTAiLCJpYXQiOjE3MjQzMDU1NDcsImV4cCI6MTgxMDcwNTU0N30.Hc88hX0fz37yHFXG31Etmfqp7JB6TxoQlG0j5bZkmPk-ITFI_Ncv4sgVawJ-7TF83_po6xBmpNOnwkGc-Jtiyg'
--- src/views/seller/terminal/index.jsx
+++ src/views/seller/terminal/index.jsx
@@ -1,51 +1,179 @@
-
 // Chakra imports
-import { Box, Button, Progress, SimpleGrid, Td, Tr } from "@chakra-ui/react";
-// import DevelopmentTable from "views/admin/dataTables/components/DevelopmentTable";
-// import CheckTable from "views/admin/dataTables/components/CheckTable";
-// import ColumnsTable from "views/admin/dataTables/components/ColumnsTable";
+import { 
+  Box, Button, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, SimpleGrid, Td, Tr, useDisclosure, useColorModeValue, Text, 
+  Progress,
+  Switch
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { FaEdit } from "react-icons/fa";
 import ComplexTable from "views/admin/dataTables/components/ComplexTable";
-// import {
-//   columnsDataDevelopment,
-//   columnsDataCheck,
-//   columnsDataColumns,
-// } from "views/admin/dataTables/variables/columnsData";
-// import tableDataDevelopment from "views/admin/dataTables/variables/tableDataDevelopment.json";
-// import tableDataCheck from "views/admin/dataTables/variables/tableDataCheck.json";
-// import tableDataColumns from "views/admin/dataTables/variables/tableDataColumns.json";
-import React from "react";
 
 export default function SellerTerminal() {
-  // Chakra Color Mode
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
+
+  // Setting input text color based on color mode
+  const inputTextColor = useColorModeValue('gray.800', 'white');
+
+  // State to manage form values and validation
+  const [formValues, setFormValues] = useState({
+    name: '',
+    account: '',
+    filialCode: '',
+    inn: ''
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    name: '',
+    account: '',
+    filialCode: '',
+    inn: ''
+  });
+
+  const resetValue = () => {
+    setFormValues({
+      name: '',
+      account: '',
+      filialCode: '',
+      inn: '',
+    });
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+
+    // Simple validation example
+    if (value.trim() === '') {
+      setFormErrors({ ...formErrors, [name]: `${name} is required` });
+    } else {
+      setFormErrors({ ...formErrors, [name]: '' });
+    }
+  };
+
+  const handleSave = () => {
+    // You can add more validation logic here
+    const errors = {};
+    Object.keys(formValues).forEach(key => {
+      if (formValues[key].trim() === '') {
+        errors[key] = `${key} is required`;
+      }
+    });
+
+    if (Object.keys(errors).length === 0) {
+      console.log("Form Submitted", formValues);
+      onClose();
+      resetValue()
+    } else {
+      setFormErrors(errors);
+    }
+  };
+
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       <SimpleGrid
-        mb='20px'
+        mb="20px"
         columns={{ sm: 1 }}
-        spacing={{ base: "20px", xl: "20px" }}>
-        {/* <DevelopmentTable
-          columnsData={columnsDataDevelopment}
-          tableData={tableDataDevelopment}
-        />
-        <CheckTable columnsData={columnsDataCheck} tableData={tableDataCheck} />
-        <ColumnsTable
-          columnsData={columnsDataColumns}
-          tableData={tableDataColumns}
-        /> */}
-        <ComplexTable name="Table" buttonChild={
-          <Button name="Add" textColor={"#fff"} color={"#fff"} background={"#000"}/>
-        } thead={['Name', 'Status', 'Date', 'Progress']}>
+        spacing={{ base: "20px", xl: "20px" }}
+      >
+        <ComplexTable 
+          name="Table" 
+          buttonChild={<Button onClick={onOpen}>Create terminal</Button>} 
+          thead={['Name', 'Inn', 'Account', 'Filial code', "Update", "Active"]}
+        >
           <Tr>
             <Td>John Doe</Td>
             <Td>Approved</Td>
             <Td>2024-08-21</Td>
+            <Td>2024-08-21</Td>
             <Td>
-              <Progress variant="table" colorScheme="brandScheme" h="8px" w="108px" value={70} />
+              <Box ms={5}>
+                <button>
+                  <FaEdit/>
+                </button>
+              </Box>
+            </Td>
+            <Td>
+              <Box>
+              <Switch colorScheme='teal' size='lg' />
+              </Box>
             </Td>
           </Tr>
-          {/* Add more rows as needed */}
         </ComplexTable>
       </SimpleGrid>
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={() => {
+          onClose()
+          resetValue()
+        }}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create your account</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl isInvalid={!!formErrors.name}>
+              <FormLabel>Name</FormLabel>
+              <Input
+                name="name"
+                ref={initialRef}
+                placeholder="Enter the terminal name"
+                value={formValues.name}
+                onChange={handleChange}
+                color={inputTextColor}
+              />
+              {formErrors.name && <Text color="red.500" fontSize="sm">{formErrors.name}</Text>}
+            </FormControl>
+            <FormControl mt={4} isInvalid={!!formErrors.account}>
+              <FormLabel>Account</FormLabel>
+              <Input
+                name="account"
+                placeholder="Enter the terminal account"
+                value={formValues.account}
+                onChange={handleChange}
+                color={inputTextColor}
+              />
+              {formErrors.account && <Text color="red.500" fontSize="sm">{formErrors.account}</Text>}
+            </FormControl>
+            <FormControl mt={4} isInvalid={!!formErrors.filialCode}>
+              <FormLabel>Filial Code</FormLabel>
+              <Input
+                name="filialCode"
+                placeholder="Enter the terminal filial code"
+                value={formValues.filialCode}
+                onChange={handleChange}
+                color={inputTextColor}
+              />
+              {formErrors.filialCode && <Text color="red.500" fontSize="sm">{formErrors.filialCode}</Text>}
+            </FormControl>
+            <FormControl mt={4} isInvalid={!!formErrors.inn}>
+              <FormLabel>Inn</FormLabel>
+              <Input
+                name="inn"
+                placeholder="Enter the terminal inn"
+                value={formValues.inn}
+                onChange={handleChange}
+                color={inputTextColor}
+              />
+              {formErrors.inn && <Text color="red.500" fontSize="sm">{formErrors.inn}</Text>}
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleSave}>
+              Save
+            </Button>
+            <Button onClick={() => {
+              onClose()
+              resetValue()
+            }}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
