3e91d39cdd3466360600d669750222585abfd1b8
Alisher <alishersodiqov09@gmail.com>
2024-09-05 14:04:43 +0500

    dashboard edit

--- src/views/admin/default/components/TotalSpent.js
+++ src/views/admin/default/components/TotalSpent.js
@@ -1,20 +1,14 @@
-// Chakra imports
 import {
   Box,
   Button,
   Flex,
   Icon,
-  Text,
   useColorModeValue,
 } from "@chakra-ui/react";
-// Custom components
 import Card from "components/card/Card.js";
 import LineChart from "components/charts/LineChart";
 import React from "react";
-import { IoCheckmarkCircle } from "react-icons/io5";
 import { MdBarChart, MdOutlineCalendarToday } from "react-icons/md";
-// Assets
-import { RiArrowUpSFill } from "react-icons/ri";
 import {
   lineChartDataTotalSpent,
   lineChartOptionsTotalSpent,
@@ -22,10 +16,6 @@ import {
 
 export default function TotalSpent(props) {
   const { ...rest } = props;
-
-  // Chakra Color Mode
-
-  const textColor = useColorModeValue("secondaryGray.900", "white");
   const textColorSecondary = useColorModeValue("secondaryGray.600", "white");
   const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
   const iconColor = useColorModeValue("brand.500", "white");
@@ -38,6 +28,7 @@ export default function TotalSpent(props) {
     { bg: "secondaryGray.300" },
     { bg: "whiteAlpha.100" }
   );
+
   return (
     <Card
       justifyContent='center'
@@ -79,45 +70,12 @@ export default function TotalSpent(props) {
         </Flex>
       </Flex>
       <Flex w='100%' flexDirection={{ base: "column", lg: "row" }}>
-        <Flex flexDirection='column' me='20px' mt='28px'>
-          <Text
-            color={textColor}
-            fontSize='34px'
-            textAlign='start'
-            fontWeight='700'
-            lineHeight='100%'>
-            $37.5K
-          </Text>
-          <Flex align='center' mb='20px'>
-            <Text
-              color='secondaryGray.600'
-              fontSize='sm'
-              fontWeight='500'
-              mt='4px'
-              me='12px'>
-              Total Spent
-            </Text>
-            <Flex align='center'>
-              <Icon as={RiArrowUpSFill} color='green.500' me='2px' mt='2px' />
-              <Text color='green.500' fontSize='sm' fontWeight='700'>
-                +2.45%
-              </Text>
-            </Flex>
-          </Flex>
-
-          <Flex align='center'>
-            <Icon as={IoCheckmarkCircle} color='green.500' me='4px' />
-            <Text color='green.500' fontSize='md' fontWeight='700'>
-              On track
-            </Text>
-          </Flex>
-        </Flex>
-        <Box minH='260px' minW='75%' mt='auto'>
-          <LineChart
-            chartData={lineChartDataTotalSpent}
-            chartOptions={lineChartOptionsTotalSpent}
-          />
-        </Box>
+          <Box minH='310px' minW='100%' mt='auto'>
+              <LineChart
+                  chartData={lineChartDataTotalSpent}
+                  chartOptions={lineChartOptionsTotalSpent}
+              />
+          </Box>
       </Flex>
     </Card>
   );
