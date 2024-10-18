7a3ccb101c59475d6b022be8ed7f9ad5fa36dac4
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-10-18 12:12:11 +0500

    ee333

--- src/views/shartlar/TermsAndConditions.jsx
+++ src/views/shartlar/TermsAndConditions.jsx
@@ -10,7 +10,7 @@ const TermsAndConditions = () => {
         Shartlar (T&C)
       </Heading>
       <Text mb={2}><strong>Oxirgi yangilangan:</strong> 14/10/2024</Text>
-      
+
       <List spacing={3} mt={4}>
         <ListItem>
           <ListIcon as={CheckCircleIcon} color="green.500" />
@@ -33,7 +33,7 @@ const TermsAndConditions = () => {
           <ListIcon as={CheckCircleIcon} color="green.500" />
           <strong>3. To'lovni bekor qilish</strong>
           <Text>
-          Foydalanuvchilar tranzaktsiyadan keyin 24 soat ichida to'lovni bekor qilishlari mumkin. Ushbu muddatdan keyin bekor qilish mumkin bo'lmaydi va xaridor va sotuvchi o'rtasidagi nizolarni hal qilish kerak bo'ladi.
+            Foydalanuvchilar tranzaktsiyadan keyin 24 soat ichida to'lovni bekor qilishlari mumkin. Ushbu muddatdan keyin bekor qilish mumkin bo'lmaydi va xaridor va sotuvchi o'rtasidagi nizolarni hal qilish kerak bo'ladi.
           </Text>
         </ListItem>
 
@@ -41,8 +41,8 @@ const TermsAndConditions = () => {
           <ListIcon as={CheckCircleIcon} color="green.500" />
           <strong>4. To'lovlar va to'lovlar</strong>
           <Text>
-          QR toʻlov ishlab chiqarish xizmatidan foydalanish uchun sotuvchilardan oylik abonent toʻlovi olinishi mumkin. Obuna tafsilotlari, jumladan, narxlar sotuvchilar roʻyxatdan oʻtgandan soʻng ular bilan baham koʻriladi.
-          To'lovlar qaytarilmaydi, biz tomonimizdagi muammolar tufayli xizmat ko'rsatishda uzilishlar yuzaga kelgan hollar bundan mustasno.
+            QR toʻlov ishlab chiqarish xizmatidan foydalanish uchun sotuvchilardan oylik abonent toʻlovi olinishi mumkin. Obuna tafsilotlari, jumladan, narxlar sotuvchilar roʻyxatdan oʻtgandan soʻng ular bilan baham koʻriladi.
+            To'lovlar qaytarilmaydi, biz tomonimizdagi muammolar tufayli xizmat ko'rsatishda uzilishlar yuzaga kelgan hollar bundan mustasno.
           </Text>
         </ListItem>
 
@@ -50,7 +50,7 @@ const TermsAndConditions = () => {
           <ListIcon as={CheckCircleIcon} color="green.500" />
           <strong>5. Ma'lumotlarning maxfiyligi </strong>
           <Text>
-          Biz Maxfiylik siyosatimizda tavsiflanganidek, foydalanuvchi ma'lumotlari, to'lov statistikasi va tranzaksiya ma'lumotlari kabi ma'lum ma'lumotlarni to'playmiz. Ushbu ilovadan foydalanish orqali foydalanuvchilar o'z ma'lumotlarini to'plash va ulardan foydalanishga rozilik bildiradilar.
+            Biz Maxfiylik siyosatimizda tavsiflanganidek, foydalanuvchi ma'lumotlari, to'lov statistikasi va tranzaksiya ma'lumotlari kabi ma'lum ma'lumotlarni to'playmiz. Ushbu ilovadan foydalanish orqali foydalanuvchilar o'z ma'lumotlarini to'plash va ulardan foydalanishga rozilik bildiradilar.
           </Text>
         </ListItem>
 
@@ -58,8 +58,8 @@ const TermsAndConditions = () => {
           <ListIcon as={CheckCircleIcon} color="green.500" />
           <strong>6. Mas'uliyatni cheklash </strong>
           <Text>
-          SBP QRPay sotuvchilar va xaridorlar o'rtasidagi ruxsatsiz bitimlar yoki kelishmovchiliklar uchun javobgar emas. Bizning rolimiz QR-ga asoslangan to'lovlar uchun platformani taqdim etish bilan cheklangan va tranzaksiyani amalga oshirishdagi har qanday xatolar ishtirokchilarning javobgarligidir.
-          Biz texnik nosozliklar, xizmat ko'rsatishdagi uzilishlar yoki ilovamizdan foydalanish natijasida yuzaga keladigan yo'qotishlar uchun javobgar emasmiz.
+            SBP QRPay sotuvchilar va xaridorlar o'rtasidagi ruxsatsiz bitimlar yoki kelishmovchiliklar uchun javobgar emas. Bizning rolimiz QR-ga asoslangan to'lovlar uchun platformani taqdim etish bilan cheklangan va tranzaksiyani amalga oshirishdagi har qanday xatolar ishtirokchilarning javobgarligidir.
+            Biz texnik nosozliklar, xizmat ko'rsatishdagi uzilishlar yoki ilovamizdan foydalanish natijasida yuzaga keladigan yo'qotishlar uchun javobgar emasmiz.
           </Text>
         </ListItem>
 
@@ -67,7 +67,7 @@ const TermsAndConditions = () => {
           <ListIcon as={CheckCircleIcon} color="green.500" />
           <strong>7. Shartlarga o'zgartirishlar </strong>
           <Text>
-          Biz ushbu shartlarni istalgan vaqtda o'zgartirish huquqini saqlab qolamiz. O'zgartirishlar ilovada e'lon qilinadi va xizmatdan doimiy foydalanish yangi shartlarni qabul qilishni anglatadi.
+            Biz ushbu shartlarni istalgan vaqtda o'zgartirish huquqini saqlab qolamiz. O'zgartirishlar ilovada e'lon qilinadi va xizmatdan doimiy foydalanish yangi shartlarni qabul qilishni anglatadi.
           </Text>
         </ListItem>
 
