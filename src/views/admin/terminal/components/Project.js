3a9f7576ba177b9fa4e7f6a0a35829528be94d58
abdulvohid31 <abdulvohidcode@gmail.com>
2024-10-23 11:29:20 +0500

    cd

--- src/views/admin/terminal/components/Project.js
+++ src/views/admin/terminal/components/Project.js
@@ -1,5 +1,10 @@
 // Chakra imports
 import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
   Box,
   Button,
   Flex,
@@ -8,65 +13,51 @@ import {
   Link,
   Text,
   useColorModeValue,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 // Custom components
-import Card from "components/card/Card.js";
-import { delete_notification } from "contexts/api";
-import { isRead_notification } from "contexts/api";
-import { globalPostFunction } from "contexts/logic-function/globalFunktion";
-import React, { useState } from "react";
-import { IoCheckmarkSharp } from "react-icons/io5";
+import Card from 'components/card/Card.js';
+import { delete_notification } from 'contexts/api';
+import { isRead_notification } from 'contexts/api';
+import { globalPostFunction } from 'contexts/logic-function/globalFunktion';
+import React, { useState } from 'react';
+import { IoCheckmarkSharp } from 'react-icons/io5';
 // Assets
-import { MdDelete, MdEdit } from "react-icons/md";
+import { MdDelete, MdEdit } from 'react-icons/md';
+import { MdNotificationsActive } from "react-icons/md";
+import { MdNotifications } from "react-icons/md";
 
 export default function Project(props) {
   const { item, image, getFunction, ...rest } = props;
   const [loading, setLoading] = useState(false);
   // Chakra Color Mode
-  const textColorPrimary = useColorModeValue("secondaryGray.900", "white");
-  const textColorSecondary = "gray.400";
-  const navbarIcon = useColorModeValue("#1B255A", "white");
-  const brandColor = useColorModeValue("brand.500", "white");
-  const bg = useColorModeValue("#e4e5f1", "navy.700");
-
+  const textColorPrimary = useColorModeValue('secondaryGray.900', 'white');
+  const textColorSecondary = 'gray.400';
+  const navbarIcon = useColorModeValue('#1B255A', 'white');
+  const brandColor = useColorModeValue('brand.500', 'white');
+  // const bg = useColorModeValue('#e4e5f1', 'navy.700');
   return (
-    <Card bg={bg} {...rest} p="14px" >
-      <Flex
-        align="center"
-        justifyContent={"space-between"}
-        alignItems={"center"}
-        direction={{ base: "column", md: "row" }}
-      >
-        <Flex>
-          <Image h="60px" w="60px" src={image} borderRadius="8px" me="20px" />
-          <Box mt={{ base: "10px", md: "0" }}>
-            <Text
-              color={textColorPrimary}
-              fontWeight="500"
-              fontSize="md"
-              mb="4px"
-            >
-              {item.title ? item.title : "Message for you!"}
-            </Text>
-            <Text
-              fontWeight="500"
-              color={textColorSecondary}
-              fontSize="sm"
-              me="4px"
-            >
-              Date • {item.createdAt.slice(0, 10)}
-              <Link
-                fontWeight="500"
-                color={brandColor}
-                fontSize="sm"
-                ms={"7px"}
-              >
-                {item.createdAt.slice(11, 16)}
-              </Link>
-            </Text>
+    <Accordion allowToggle border={item.isRead || '1px solid green'} mb={'10px'}>
+      <AccordionItem>
+        <AccordionButton>
+          {item.isRead ? <MdNotifications size={30} /> : <MdNotificationsActive size={30} />}
+          <Box flex="1" p={'10px'} textAlign="left">
+            {item.title ? item.title : 'Message for you!'}
           </Box>
-        </Flex>
-        <Flex>
+          <AccordionIcon />
+          <Button
+            onClick={() =>
+              globalPostFunction({
+                url: delete_notification,
+                postData: { ids: [item.id] },
+                setLoading: setLoading,
+                getFunction: getFunction,
+              })
+            }
+            variant="no-hover"
+            bg="transparent"
+          >
+            <MdDelete color={navbarIcon} size={23} />
+          </Button>
           <Button
             onClick={() =>
               globalPostFunction({
@@ -82,22 +73,48 @@ export default function Project(props) {
           >
             <IoCheckmarkSharp color={navbarIcon} size={23} />
           </Button>
-          <Button
-            onClick={() =>
-              globalPostFunction({
-                url: delete_notification,
-                postData: { ids: [item.id] },
-                setLoading: setLoading,
-                getFunction: getFunction,
-              })
-            }
-            variant="no-hover"
-            bg="transparent"
-          >
-            <MdDelete color={navbarIcon} size={23} />
-          </Button>
-        </Flex>
-      </Flex>
-    </Card>
+        </AccordionButton>
+        <AccordionPanel pb={4}>
+          {/* <Card {...rest} p="14px"> */}
+            <Flex
+              align="center"
+              justifyContent={'space-between'}
+              alignItems={'center'}
+              direction={{ base: 'column', md: 'row' }}
+            >
+              <Flex alignItems={'center'} p={'20px'} >
+                {/* <Image h="60px" w="60px" src={image} borderRadius="8px" me="20px" /> */}
+                <Box mt={{ base: '10px', md: '0' }}>
+                  <Text
+                    color={textColorPrimary}
+                    fontWeight="500"
+                    fontSize="md"
+                    mb="4px"
+                  >
+                    {item.title ? item.title : 'Message for you!'}
+                  </Text>
+                  <Text
+                    fontWeight="500"
+                    color={textColorSecondary}
+                    fontSize="sm"
+                    me="4px"
+                  >
+                    Date • {item.createdAt.slice(0, 10)}
+                    <Link
+                      fontWeight="500"
+                      color={brandColor}
+                      fontSize="sm"
+                      ms={'7px'}
+                    >
+                      {item.createdAt.slice(11, 16)}
+                    </Link>
+                  </Text>
+                </Box>
+              </Flex>
+            </Flex>
+          {/* </Card> */}
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
   );
 }
